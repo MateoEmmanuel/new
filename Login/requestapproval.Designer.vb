76384167c19743a5e0532b28524d124f32e106cc
ArataUzumaki <mateoemmanuelwalter10@gmail.com>
2024-10-24 15:17:52 +0800

    Updated (10/24/2024-3:17)

--- Login/requestapproval.Designer.vb
+++ Login/requestapproval.Designer.vb
@@ -23,16 +23,23 @@ Partial Class requestapproval
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.DGVschedules = New System.Windows.Forms.DataGridView()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
         Me.btnback = New System.Windows.Forms.Button()
         Me.btnmodify = New System.Windows.Forms.Button()
         Me.btndelete = New System.Windows.Forms.Button()
         Me.btnadd = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.DGVrequest = New System.Windows.Forms.DataGridView()
+        Me.TableLayoutPanel4 = New System.Windows.Forms.TableLayoutPanel()
+        Me.DGVschedules = New System.Windows.Forms.DataGridView()
+        Me.btnrequestreload = New System.Windows.Forms.Button()
+        Me.btnlistreload = New System.Windows.Forms.Button()
         Me.TableLayoutPanel1.SuspendLayout()
-        CType(Me.DGVschedules, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TableLayoutPanel2.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
+        CType(Me.DGVrequest, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel4.SuspendLayout()
+        CType(Me.DGVschedules, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
@@ -42,43 +49,20 @@ Partial Class requestapproval
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.DGVschedules, 1, 2)
-        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 1, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel3, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel4, 1, 1)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 5
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.69438!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 64.54768!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.43032!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.81174!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.75795!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.TableLayoutPanel1.Size = New System.Drawing.Size(800, 450)
         Me.TableLayoutPanel1.TabIndex = 1
-        '
-        'DGVschedules
-        '
-        Me.DGVschedules.BackgroundColor = System.Drawing.Color.DodgerBlue
-        Me.DGVschedules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DGVschedules.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DGVschedules.Location = New System.Drawing.Point(23, 124)
-        Me.DGVschedules.Name = "DGVschedules"
-        Me.DGVschedules.Size = New System.Drawing.Size(754, 258)
-        Me.DGVschedules.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.NavajoWhite
-        Me.Label1.Location = New System.Drawing.Point(23, 82)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(754, 39)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Requested Schedules"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft
         '
         'TableLayoutPanel2
         '
@@ -98,6 +82,7 @@ Partial Class requestapproval
         Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
         Me.TableLayoutPanel2.RowCount = 1
         Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.TableLayoutPanel2.Size = New System.Drawing.Size(754, 38)
         Me.TableLayoutPanel2.TabIndex = 2
         '
@@ -161,6 +146,92 @@ Partial Class requestapproval
         Me.btnadd.Text = "Add"
         Me.btnadd.UseVisualStyleBackColor = False
         '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.ColumnCount = 2
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.DGVschedules, 0, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.DGVrequest, 0, 0)
+        Me.TableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(23, 172)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 1
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(754, 210)
+        Me.TableLayoutPanel3.TabIndex = 3
+        '
+        'DGVrequest
+        '
+        Me.DGVrequest.BackgroundColor = System.Drawing.Color.DodgerBlue
+        Me.DGVrequest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGVrequest.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DGVrequest.Location = New System.Drawing.Point(3, 3)
+        Me.DGVrequest.Name = "DGVrequest"
+        Me.DGVrequest.Size = New System.Drawing.Size(371, 204)
+        Me.DGVrequest.TabIndex = 1
+        '
+        'TableLayoutPanel4
+        '
+        Me.TableLayoutPanel4.ColumnCount = 2
+        Me.TableLayoutPanel4.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel4.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel4.Controls.Add(Me.btnlistreload, 0, 1)
+        Me.TableLayoutPanel4.Controls.Add(Me.btnrequestreload, 0, 1)
+        Me.TableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel4.Location = New System.Drawing.Point(23, 23)
+        Me.TableLayoutPanel4.Name = "TableLayoutPanel4"
+        Me.TableLayoutPanel4.RowCount = 2
+        Me.TableLayoutPanel4.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel4.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel4.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel4.Size = New System.Drawing.Size(754, 143)
+        Me.TableLayoutPanel4.TabIndex = 4
+        '
+        'DGVschedules
+        '
+        Me.DGVschedules.BackgroundColor = System.Drawing.Color.DodgerBlue
+        Me.DGVschedules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGVschedules.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DGVschedules.Location = New System.Drawing.Point(380, 3)
+        Me.DGVschedules.Name = "DGVschedules"
+        Me.DGVschedules.Size = New System.Drawing.Size(371, 204)
+        Me.DGVschedules.TabIndex = 2
+        '
+        'btnrequestreload
+        '
+        Me.btnrequestreload.AutoSize = True
+        Me.btnrequestreload.BackColor = System.Drawing.Color.Transparent
+        Me.btnrequestreload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnrequestreload.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnrequestreload.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnrequestreload.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnrequestreload.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.btnrequestreload.Location = New System.Drawing.Point(3, 74)
+        Me.btnrequestreload.Name = "btnrequestreload"
+        Me.btnrequestreload.Size = New System.Drawing.Size(371, 66)
+        Me.btnrequestreload.TabIndex = 7
+        Me.btnrequestreload.Text = "Request Schedules"
+        Me.btnrequestreload.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnrequestreload.UseVisualStyleBackColor = False
+        '
+        'btnlistreload
+        '
+        Me.btnlistreload.BackColor = System.Drawing.Color.Transparent
+        Me.btnlistreload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnlistreload.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnlistreload.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnlistreload.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnlistreload.ForeColor = System.Drawing.Color.NavajoWhite
+        Me.btnlistreload.Location = New System.Drawing.Point(380, 74)
+        Me.btnlistreload.Name = "btnlistreload"
+        Me.btnlistreload.Size = New System.Drawing.Size(371, 66)
+        Me.btnlistreload.TabIndex = 8
+        Me.btnlistreload.Text = "Schedule Lists"
+        Me.btnlistreload.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnlistreload.UseVisualStyleBackColor = False
+        '
         'requestapproval
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -177,19 +248,26 @@ Partial Class requestapproval
         Me.Text = "requestapproval"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.TableLayoutPanel1.ResumeLayout(False)
-        Me.TableLayoutPanel1.PerformLayout()
-        CType(Me.DGVschedules, System.ComponentModel.ISupportInitialize).EndInit()
         Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        CType(Me.DGVrequest, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel4.ResumeLayout(False)
+        Me.TableLayoutPanel4.PerformLayout()
+        CType(Me.DGVschedules, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
-    Friend WithEvents DGVschedules As DataGridView
-    Friend WithEvents Label1 As Label
     Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
     Friend WithEvents btnback As Button
     Friend WithEvents btnmodify As Button
     Friend WithEvents btndelete As Button
     Friend WithEvents btnadd As Button
+    Friend WithEvents TableLayoutPanel3 As TableLayoutPanel
+    Friend WithEvents DGVrequest As DataGridView
+    Friend WithEvents DGVschedules As DataGridView
+    Friend WithEvents TableLayoutPanel4 As TableLayoutPanel
+    Friend WithEvents btnlistreload As Button
+    Friend WithEvents btnrequestreload As Button
 End Class
--- Login/requestapproval.vb
+++ Login/requestapproval.vb
@@ -8,8 +8,10 @@ Public Class requestapproval
         If conn.State = ConnectionState.Open Then
             conn.Close()
         End If
-        tableloader()
+        tableloader_request()
+        tableloader_schedules()
         FormatDataGridViews_schedules()
+        FormatDataGridViews_request()
     End Sub
 
     Private Sub btnadd_Click(sender As Object, e As EventArgs) Handles btnadd.Click
@@ -17,8 +19,82 @@ Public Class requestapproval
     End Sub
 
     Private Sub btndelete_Click(sender As Object, e As EventArgs) Handles btndelete.Click
+        ' Check if a row is selected in the DGVrequest DataGridView (requests table)
+        If DGVrequest.SelectedRows.Count > 0 Then
+            ' Retrieve the requestID from the selected row (assumed to be in the 5th column)
+            Dim selectedRequestID As Integer = Convert.ToInt32(DGVrequest.SelectedRows(0).Cells("requestID").Value)
 
+            ' Confirmation prompt for request deletion
+            Dim result As DialogResult = MessageBox.Show("Are you sure you want to delete the selected request?", "Confirm Deletion",
+                                                     MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+
+            If result = DialogResult.Yes Then
+                ' Delete the record from the database using requestID
+                Dim query As String = "DELETE FROM requests WHERE requestID = @requestID"
+                Dim cmd As New MySqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@requestID", selectedRequestID)
+
+                Try
+                    If conn.State = ConnectionState.Closed Then
+                        conn.Open()
+                    End If
+
+                    cmd.ExecuteNonQuery()
+
+                    ' Refresh the table to show updated data
+                    tableloader_request()
+                    tableloader_schedules()
+                    FormatDataGridViews_schedules()
+                    FormatDataGridViews_request()
+
+                    MessageBox.Show("Request deleted successfully.")
+                Catch ex As Exception
+                    MessageBox.Show("Error deleting request: " & ex.Message)
+                Finally
+                    conn.Close()
+                End Try
+            End If
+
+            ' Check if a row is selected in the DGVschedules DataGridView (schedules table)
+        ElseIf DGVschedules.SelectedRows.Count > 0 Then
+            ' Retrieve the shed_id from the selected row (assumed to be in the 5th column)
+            Dim selectedShedID As Integer = Convert.ToInt32(DGVschedules.SelectedRows(0).Cells("shed_id").Value)
+
+            ' Confirmation prompt for schedule deletion
+            Dim result As DialogResult = MessageBox.Show("Are you sure you want to delete the selected schedule?", "Confirm Deletion",
+                                                     MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+
+            If result = DialogResult.Yes Then
+                ' Delete the record from the sched table using shed_id
+                Dim query As String = "DELETE FROM sched WHERE shed_id = @shed_id"
+                Dim cmd As New MySqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@shed_id", selectedShedID)
+
+                Try
+                    If conn.State = ConnectionState.Closed Then
+                        conn.Open()
+                    End If
+
+                    cmd.ExecuteNonQuery()
+
+                    ' Refresh the table to show updated data
+                    tableloader_request()
+                    tableloader_schedules()
+                    FormatDataGridViews_schedules()
+                    FormatDataGridViews_request()
+
+                    MessageBox.Show("Schedule deleted successfully.")
+                Catch ex As Exception
+                    MessageBox.Show("Error deleting schedule: " & ex.Message)
+                Finally
+                    conn.Close()
+                End Try
+            End If
+        Else
+            MessageBox.Show("Please select a row to delete.")
+        End If
     End Sub
+
 
     Private Sub btnmodify_Click(sender As Object, e As EventArgs) Handles btnmodify.Click
 
@@ -28,8 +104,8 @@ Public Class requestapproval
         Admin.Show()
         Me.Hide()
     End Sub
-    Private Sub tableloader()
-        Dim query As String = "SELECT request_d, request_t, room, request FROM requests" ' Use roomlist if you want to get room status from that table
+    Private Sub tableloader_request()
+        Dim query As String = "SELECT request_d, request_t, room, request, requestID FROM requests" ' Use roomlist if you want to get room status from that table
         Dim adapter As New MySqlDataAdapter(query, conn)
         Dim table As New DataTable()
 
@@ -38,20 +114,21 @@ Public Class requestapproval
             adapter.Fill(table)
 
             ' Bind the DataTable to the DataGridView
-            DGVschedules.DataSource = table
+            DGVrequest.DataSource = table
         Catch ex As Exception
             MessageBox.Show("Error retrieving room statuses: " & ex.Message)
         End Try
     End Sub
 
-    Private Sub FormatDataGridViews_schedules()
+    Private Sub FormatDataGridViews_request()
         ' Format the schedules DataGridView
-        If DGVschedules.Rows.Count > 0 Then ' Check if there are rows
-            With DGVschedules
+        If DGVrequest.Rows.Count > 0 Then ' Check if there are rows
+            With DGVrequest
                 .Columns(0).HeaderText = "Date"
                 .Columns(1).HeaderText = "Time"
                 .Columns(2).HeaderText = "Room"
                 .Columns(3).HeaderText = "Request Message"
+                .Columns(4).HeaderText = "Request ID"
 
                 ' First, adjust the columns based on data length
                 .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
@@ -60,11 +137,12 @@ Public Class requestapproval
                 .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
             End With
         Else
-            With DGVschedules
+            With DGVrequest
                 .Columns(0).HeaderText = "Date"
                 .Columns(1).HeaderText = "Time"
                 .Columns(2).HeaderText = "Room"
                 .Columns(3).HeaderText = "Request Message"
+                .Columns(4).HeaderText = "Request ID"
 
                 ' No auto-size adjustment if no data
                 .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
@@ -72,4 +150,59 @@ Public Class requestapproval
         End If
     End Sub
 
+    Private Sub tableloader_schedules()
+        Dim query As String = "SELECT room_time, room_name, room_code, building_letter, shed_id FROM sched" ' Use roomlist if you want to get room status from that table
+        Dim adapter As New MySqlDataAdapter(query, conn)
+        Dim table As New DataTable()
+
+        Try
+            ' Fill the DataTable with data from the sched table
+            adapter.Fill(table)
+
+            ' Bind the DataTable to the DataGridView
+            DGVrequest.DataSource = table
+        Catch ex As Exception
+            MessageBox.Show("Error retrieving room statuses: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub FormatDataGridViews_schedules()
+        ' Format the schedules DataGridView
+        If DGVrequest.Rows.Count > 0 Then ' Check if there are rows
+            With DGVrequest
+                .Columns(0).HeaderText = "Time"
+                .Columns(1).HeaderText = "Room"
+                .Columns(2).HeaderText = "Room Code"
+                .Columns(3).HeaderText = "Building"
+                .Columns(4).HeaderText = "Schedule ID"
+
+                ' First, adjust the columns based on data length
+                .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+
+                ' Then, set it back to Fill for a balanced layout
+                .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            End With
+        Else
+            With DGVrequest
+                .Columns(0).HeaderText = "Time"
+                .Columns(1).HeaderText = "Room"
+                .Columns(2).HeaderText = "Room Code"
+                .Columns(3).HeaderText = "Building"
+                .Columns(4).HeaderText = "Schedule ID"
+
+                ' No auto-size adjustment if no data
+                .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            End With
+        End If
+    End Sub
+
+    Private Sub btnrequestreload_Click(sender As Object, e As EventArgs) Handles btnrequestreload.Click
+        tableloader_request()
+        FormatDataGridViews_request()
+    End Sub
+
+    Private Sub btnlistreload_Click(sender As Object, e As EventArgs) Handles btnlistreload.Click
+        tableloader_schedules()
+        FormatDataGridViews_schedules()
+    End Sub
 End Class
